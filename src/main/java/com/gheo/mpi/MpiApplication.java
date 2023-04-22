37cf8e9c50226d5ba6cb3542db4116cd8b7df5f6
gheo <gheorghe.coanta@gmail.com>
2023-04-22 16:40:15 +0300

    INIT proiect

---
+++ src/main/java/com/gheo/mpi/MpiApplication.java
@@ -0,0 +1,13 @@
+package com.gheo.mpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpiApplication.class, args);
+	}
+
+}
